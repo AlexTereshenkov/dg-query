f3292e4b4f115ac6d440131d775afb4d246eacb1
Alexey Tereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2024-09-28 19:56:13 +0200

    go: add Go pipeline build stage

---
+++ .buildkite/build-go.sh
@@ -0,0 +1,39 @@
+#! /bin/bash -xue
+echo --- 'Prepare environment'
+sudo apt-get install wget jq -y
+wget https://dl.google.com/go/go1.22.7.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.22.7.linux-amd64.tar.gz
+
+echo --- 'Check Go installation'
+export GOROOT="/usr/local/go"
+# setting GOBIN is required for `go install` output location
+export GOBIN="/usr/local/go/bin/"
+export PATH="$GOROOT/bin:$PATH"
+go version
+go env
+
+echo --- 'Static checks'
+go install golang.org/x/tools/cmd/goimports@latest
+go install honnef.co/go/tools/cmd/staticcheck@latest
+ls /usr/local/go/bin
+
+if [ -n "$(gofmt -l -s .)" ]; then
+    echo "Code needs to be formatted"
+    exit 1
+fi
+
+if [ -n "$(goimports -l .)" ]; then
+    echo "Imports need to be formatted"
+    exit 1
+fi
+
+staticcheck ./...
+
+echo --- 'Build project'
+go build
+go test ./...
+
+echo --- 'Sanity check'
+go run main.go deps --dg='examples/dg-real.json' foo.py spam.py
+go run main.go rdeps --dg='examples/dg-real.json' eggs.py
+go run main.go metrics --dg="examples/dg-real-transitive.json" --metric=deps-transitive | jq
