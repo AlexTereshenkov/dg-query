b7095b2dfcc794153766ebbf8efde7092b13b374
AlexTereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2025-01-08 21:14:41 +0100

    bazel: use remote cache of BuildBuddy

--- Makefile
+++ Makefile
@@ -12,12 +12,14 @@ run-go:
 	go run main.go dependencies --dg="examples/dg-real.json" foo.py spam.py
 
 build-bazel:
+	build-support/create-bazelrc-ci.sh
 	bazel run //:gazelle
 	bazel run //:buildifier
 	bazel run //:update-deps
 	bazel run //:dg-query
 	bazel build //... && bazel-bin/dg-query_/dg-query dependencies --dg="examples/dg-real.json" foo.py spam.py
 	bazel test //...
+	rm .bazelrc.ci
 
 codecov:
 	bazel coverage //... --combined_report=lcov
---
+++ build-support/create-bazelrc-ci.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "common --show_timestamps" > .bazelrc.ci
+echo "build --build_metadata=ROLE=CI" >> .bazelrc.ci
+
+# this secret is declared in Buildkite secrets
+if command -v buildkite-agent &> /dev/null
+then
+    # runs in Buildkite
+    BUILD_BUDDY_API_KEY=$(buildkite-agent secret get BUILD_BUDDY_API_KEY)
+else
+    # runs locally and the api key should be available via the environment variable
+    BUILD_BUDDY_API_KEY=${BUILD_BUDDY_API_KEY}
+fi
+echo "build --remote_header=x-buildbuddy-api-key=$BUILD_BUDDY_API_KEY" >> .bazelrc.ci
