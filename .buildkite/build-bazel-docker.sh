08cfc43bbb8b2159ea777a003666de438683ac5c
AlexTereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2025-04-03 22:40:25 +0200

    bazel: do not use BuildBuddy when building in Docker container

--- .buildkite/build-bazel-docker.sh
+++ .buildkite/build-bazel-docker.sh
@@ -3,18 +3,22 @@ set -xueo pipefail
 
 echo '--- Build'
 bazel --version
-build-support/create-bazelrc-ci.sh
+
+echo "common --show_timestamps" > .bazelrc.ci
+echo "build --build_metadata=ROLE=CI" >> .bazelrc.ci
+
 # `--load` ensures that the image built is loaded into the local Docker environment, 
 # making it immediately available to run or reference by tag; otherwise, the image 
 # will only exist in Docker's build cache.
 docker build -t go-build-env -f platforms/Dockerfile --load .
 docker image ls
-bazel build //... --spawn_strategy=docker --experimental_enable_docker_sandbox --platforms="//platforms:docker"
+
+bazel build --config="docker" //... 
 
 echo '--- Test'
-bazel test //...  --spawn_strategy=docker --experimental_enable_docker_sandbox --platforms="//platforms:docker" | sed 's/^=== //' | sed 's/^--- //'
+bazel test --config="docker" //...  | sed 's/^=== //' | sed 's/^--- //'
 
 echo '--- Running a compiled CGo binary'
-bazel run //platforms/compiled:cgo-compiled
+bazel run --config="docker" //platforms/compiled:cgo-compiled
 
 echo '--- Finish'
