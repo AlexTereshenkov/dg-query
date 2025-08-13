e97ceb68ece6a5c06b932b627a008d6011579449
Alexey Tereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2025-08-13 07:39:24 +0200

    ci: add pipeline step for rules_img builds

---
+++ .buildkite/build-bazel-rules-img.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+set -xueo pipefail
+
+echo '--- Build'
+bazel --version
+build-support/create-bazelrc-ci.sh
+
+echo '--- Start Docker registry'
+docker run -d -p 5000:5000 --name registry registry:2
+curl http://localhost:5000/v2/
+
+echo '--- Build Docker image'
+bazel build //:app_image
+
+echo '--- Push Docker image to local registry'
+bazel run //:push
+
+echo '--- Check the list of images'
+curl http://localhost:5000/v2/repository/my-project/tags/list
+
+echo '--- Run the app'
+docker run --rm localhost:5000/repository/my-project:latest bash -c "/app/bin/dg-query"
+
+echo '--- Finish'
