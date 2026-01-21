9ed1c3f2f078688148fd51b9741cfa6ef0d7800a
Alexey Tereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2026-01-21 14:02:12 +0100

    Fix

--- tools/tools.go
+++ tools/tools.go
@@ -1,3 +1,5 @@
+//go:build tools
+
 /*
 This file lists tools as direct dependencies even though
 they are imported in Go source code; this is to ensure
@@ -6,9 +8,8 @@ This is necessary since `go.mod` tracks direct dependencies
 (so we add dummy `import package-name` in a `.go` file).
 */
 
-//go:build tools
 package tools
 
 import (
-    _ "golang.org/x/tools/cmd/goimports"
+	_ "golang.org/x/tools/cmd/goimports"
 )
