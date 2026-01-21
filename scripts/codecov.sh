9109afe523ebb29f4ef731cb9f8a09a82ab4491a
Alexey Tereshenkov <50622389+AlexTereshenkov@users.noreply.github.com>
2026-01-21 21:58:55 +0100

    Fix

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # This script is supposed to run after the `bazel coverage //... --combined_report=lcov` command
 # as it expects `bazel-out/_coverage/_coverage_report.dat` to exist on disk and it is not recommended
-# to run Bazel commands from within Bazel
+# to run Bazel commands from within Bazel.
+# Pure Bazel approach would be:
+# bazel coverage --instrument_test_targets --combined_report=lcov //...
+# bazel run @lcov//:genhtml -- --source-directory $(pwd) --branch-coverage --output $(pwd)/genhtml \
+#   $(bazel info output_path)/_coverage/_coverage_report.dat
 echo "Running codecov.sh Shell script to generate the HTML code coverage report"
 
 # Define the path to the genhtml binary inside the extracted lcov package
